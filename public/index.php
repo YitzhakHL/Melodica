5cbd7e93baa071dadf8fe40991b12af245e106de
YitzhakHL <bountyhunter172.2@gmail.com>
2026-04-17 12:10:46 -0600

    Added script to run develoment server. Basic script to run a PHP server for testing.

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+# Global config
+define('BASE_PATH', dirname(__DIR__) . '/');
+# Folders
+define('CORE_PATH', BASE_PATH . 'core/');
+define('APP_PATH', BASE_PATH . 'app/');
+define('PUBLIC_PATH', BASE_PATH . 'public/');
+
